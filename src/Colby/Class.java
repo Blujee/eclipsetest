b44dbd5f20049547436c4ec2ee4a74faee055a90
colby.ekins <colby.ekins@CLC-LAB209-06.TCSD.tooelesd.org>
2018-09-04 13:32:03 -0600

    first commit

---
+++ src/Colby/Class.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Colby;
+
+/**
+ * @author colby.ekins
+ *9/4
+ *Hello World
+ */
+public class Class {
+
+	public Class() {
+
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
